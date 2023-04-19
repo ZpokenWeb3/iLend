b2e9ab72e0ea0e21fbac34e5eedcb4f107105a44
Dmytro Shashkevych <shashkevich41@gmail.com>
2023-04-19 14:42:13 +0300

    update schema

--- scripts/injective_instantiate.sh
+++ scripts/injective_instantiate.sh
@@ -1,13 +1,13 @@
 # step 1 query CODE_ID from deployed contract via command
-#readonly DEPLOYMENT_TX_HASH="D8203B1130D72A61E7A91FCA2464DD4B28F220AC1EE6703CAACAD88F440EED1C"
+#readonly DEPLOYMENT_TX_HASH="E2029FC478B43D8A813DBF5920B9C4869EA49FA4742BD380A86A0AB74CE8FDE1"
 #injectived query tx $DEPLOYMENT_TX_HASH --node=https://k8s.testnet.tm.injective.network:443
 
-CODE_ID="712"
+CODE_ID="713"
 INJ_ADDRESS="inj1lsuerzge89tyd4p2pj8wrj903v5ja5emmugntd"
 
 
 # shellcheck disable=SC2089
-INIT='{"admin":"inj1lsuerzge89tyd4p2pj8wrj903v5ja5emmugntd","supported_tokens": [["inj1jyldpwc5ycuj5nn7tg7wejfs62pvxsy0l9n9xs", "inj1hwsuf0n59cm6mdhzd7rn0v79klu4jnugca0nxy"],["inj16d2fzkwzj2z39p6km9x5gk45r729lx5qk3u95t", "inj15h4pkwy9mdcz6hwahcdfuuhwfkl6jwmzerwthw"],["inj10merj58djsdq82xqq30vyvlxjzsan2dswuzfr9", "inj1m52r8r9hh8ut7n84a88vjrqxahyvw76reulxel"],["inj1ee8gx5k0qmq0aywkm6dgaxdqnys2qw8vfajaxn", "inj1qh6zj4j28xtmpqel2el4wjavpef9vmyahzsp0l"]],"pyth_contract_addr": "inj1z60tg0tekdzcasenhuuwq3htjcd5slmgf7gpez"}'
+INIT='{"admin":"inj1lsuerzge89tyd4p2pj8wrj903v5ja5emmugntd","supported_tokens": [["inj1jyldpwc5ycuj5nn7tg7wejfs62pvxsy0l9n9xs", "inj1hwsuf0n59cm6mdhzd7rn0v79klu4jnugca0nxy"],["inj16d2fzkwzj2z39p6km9x5gk45r729lx5qk3u95t", "inj15h4pkwy9mdcz6hwahcdfuuhwfkl6jwmzerwthw"],["inj10merj58djsdq82xqq30vyvlxjzsan2dswuzfr9", "inj1m52r8r9hh8ut7n84a88vjrqxahyvw76reulxel"],["inj1ee8gx5k0qmq0aywkm6dgaxdqnys2qw8vfajaxn", "inj1qh6zj4j28xtmpqel2el4wjavpef9vmyahzsp0l"]]}'
 
 
 # shellcheck disable=SC2046
--- scripts/query_contract_name_after_instantiating.sh
+++ scripts/query_contract_name_after_instantiating.sh
@@ -1,11 +1,13 @@
 # get the contract address from the response
-#CODE_ID="712"
+#CODE_ID="713"
 #injectived query wasm list-contract-by-code $CODE_ID --node=https://k8s.testnet.tm.injective.network:443 --output json
 
-CONTRACT="inj1e64k5v82ue08dqzphgdwtrme8s8v04r6hr6e7q"
+CONTRACT="inj1u6jq52m9vfkedzka40fvnchddmcsfvu655lhp2"
 readonly INJ_ADDRESS="inj1lsuerzge89tyd4p2pj8wrj903v5ja5emmugntd"
 
 ## query arbitrary balance
 BALANCE_QUERY='{"get_deposit": {"address": "'$(echo $INJ_ADDRESS)'", "denom": "inj1jyldpwc5ycuj5nn7tg7wejfs62pvxsy0l9n9xs"}}'
 injectived query wasm contract-state smart $CONTRACT "$BALANCE_QUERY" --node=https://k8s.testnet.tm.injective.network:443 --output json
+
+
 
