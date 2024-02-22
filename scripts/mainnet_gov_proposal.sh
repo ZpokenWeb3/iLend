a21be75295a8c50eb8c1e55bd5f858e56ef28828
Dmytro Shashkevych <shashkevich41@gmail.com>
2024-02-22 19:41:27 +0200

    Mainnet Gov Proposal Change

--- scripts/mainnet_gov_proposal.sh
+++ scripts/mainnet_gov_proposal.sh
@@ -1,5 +1,5 @@
 injectived tx wasm submit-proposal wasm-store ../artifacts/lending-aarch64.wasm \
---title "iLend contract upgrade (admin functions for markets management)" \
+--title "iLend v1.3 smart contract upgrade for LST assets" \
 --summary "iLend is a decentralized finance protocol developed on the Injective network.\n It offers the ability to engage in lending and borrowing activities in a decentralized, transparent, and efficient manner. The protocol leverages Injective Protocol's ability to offer fast, secure, and EVM-compatible DeFi transactions across multiple blockchain ecosystems.\n \n Current upgrade provides new admin functions to add new markets for supply and borrow and liquidation function upgrade that allows any user to be a liquidator.\n Github: https://github.com/i-Lend-org/iLend-smart-contracts\n Built by zpoken team https://zpoken.io/" \
 --instantiate-anyof-addresses inj19ae4ukagwrlprva55q9skskunv5ve7sr6myx7z \
 --deposit=10000000000000000000inj \
